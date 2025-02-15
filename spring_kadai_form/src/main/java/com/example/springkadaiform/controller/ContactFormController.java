7d7e2c56ce06c5128ca8309ba41b840ba6a63a65
近藤　望 <li2xue3mei2@gmail.com>
2025-02-15 18:38:48 +0900

    リダイレクトへ修正

--- spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -1,6 +1,6 @@
 package com.example.springkadaiform.controller;
 
-//import org.springframework.core.Conventions;
+import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,39 +16,41 @@ public class ContactFormController {
 		
     // GETリクエストでフォーム画面を表示
 	@GetMapping("/form")
-  
+
 	 public String showForm(Model model) {
-        model.addAttribute("contactForm", new ContactForm()); // ここで追加
-        return "contactFormView";
+        // すでにインスタンスが存在する場合は行わない
+        if (!model.containsAttribute("contactForm")) {
+            // ビューにフォームクラスのインスタンスを渡す
+            model.addAttribute("contactForm", new ContactForm());
+        }
+                return "contactFormView";
     }
 
 // POSTリクエストでバリデーション実施
     @PostMapping("/confirm")
-    public String confirm(RedirectAttributes redirectAttributes,
-            @Validated ContactForm form, BindingResult result ,Model model) {
+    public String confirm(
+    		@Validated ContactForm form,     		
+            BindingResult result ,
+            RedirectAttributes redirectAttributes,
+            Model model
+           ) {
     	System.out.println("postメソッド");
-    	
+    	 //
     	 // バリデーションエラーがあれば、フォーム画面に戻す
         if (result.hasErrors())  {
         	System.out.println("バリデーションチェックエラー");
             // バリデーションエラーがあれば、フォーム画面に戻す
         	// フォームクラスをビューに受け渡す
-            //redirectAttributes.addFlashAttribute("contactForm", form);
+            redirectAttributes.addFlashAttribute("contactForm", form);
             // バリデーション結果をビューに受け渡す
-            //redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX
-            		//+ Conventions.getVariableName(form), result);
-            		/*+ "contactForm",result);*/
-        	
-        	/*
-        	 * 課題で「バリデーションNG：元のフォームへリダイレクト」と記載があるが
-        	 * return "redirect:/contactFormView"の形をとると
-        	 * リロードがかかり、バリデーションの結果を保持できない
-        	 */
-            return "contactFormView"; //Formへリダイレクト redirect:/
+           redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX
+            		+ Conventions.getVariableName(form), result);            		      	
+        	        	
+            return "redirect:/form"; //Formへリダイレクト
         }
         // バリデーションOKの場合は、確認画面に遷移
         System.out.println("バリデーションOK");
-        model.addAttribute("contactForm", form);
+       model.addAttribute("contactForm", form);
     return "confirmView";
 }
 }
